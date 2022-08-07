c114ff4146981330a03d598ec55c563c98d972ab
Anurag <anuraglama5@gmail.com>
2022-08-08 00:03:23 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,32 +23,32 @@ function categoryExtend(categoryType){
     if (categoryType=="book"){
         if (categoryBookExtended==0){
             document.getElementById("bookCategory").style.maxHeight="100%";
-            document.getElementById("categoriesExtenderBar2").style.transform="rotate(0)";
-            document.getElementById("categoriesExtenderBar1").style.backgroundColor="darkturquoise";
-            document.getElementById("categoriesExtenderBar2").style.backgroundColor="darkturquoise";
+            document.getElementById("bookCategoryIcon2").style.transform="rotate(0)";
+            document.getElementById("bookCategoryIcon1").style.backgroundColor="darkturquoise";
+            document.getElementById("bookCategoryIcon2").style.backgroundColor="darkturquoise";
             categoryBookExtended=1;
         }
         else{
             document.getElementById("bookCategory").style.maxHeight="0";
-            document.getElementById("categoriesExtenderBar2").style.transform="rotate(90deg)";
-            document.getElementById("categoriesExtenderBar1").style.backgroundColor="black";
-            document.getElementById("categoriesExtenderBar2").style.backgroundColor="black";
+            document.getElementById("bookCategoryIcon2").style.transform="rotate(90deg)";
+            document.getElementById("bookCategoryIcon1").style.backgroundColor="black";
+            document.getElementById("bookCategoryIcon2").style.backgroundColor="black";
             categoryBookExtended=0;
         }
     }
     if (categoryType=="author"){
         if (categoryAuthorExtended==0){
             document.getElementById("authorCategory").style.maxHeight="100%";
-            document.getElementById("categoriesExtenderBar2").style.transform="rotate(0)";
-            document.getElementById("categoriesExtenderBar1").style.backgroundColor="darkturquoise";
-            document.getElementById("categoriesExtenderBar2").style.backgroundColor="darkturquoise";
+            document.getElementById("authorCategoryIcon2").style.transform="rotate(0)";
+            document.getElementById("authorCategoryIcon1").style.backgroundColor="darkturquoise";
+            document.getElementById("authorCategoryIcon2").style.backgroundColor="darkturquoise";
             categoryAuthorExtended=1;
         }
         else{
             document.getElementById("authorCategory").style.maxHeight="0";
-            document.getElementById("categoriesExtenderBar2").style.transform="rotate(90deg)";
-            document.getElementById("categoriesExtenderBar1").style.backgroundColor="black";
-            document.getElementById("categoriesExtenderBar2").style.backgroundColor="black";
+            document.getElementById("authorCategoryIcon2").style.transform="rotate(90deg)";
+            document.getElementById("authorCategoryIcon1").style.backgroundColor="black";
+            document.getElementById("authorCategoryIcon2").style.backgroundColor="black";
             categoryAuthorExtended=0;
         }
     }
