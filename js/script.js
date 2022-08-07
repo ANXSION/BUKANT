d701883bf01347199c509f0c1a0b13138c9c76ce
Anurag <anuraglama5@gmail.com>
2022-08-07 21:57:12 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ var menuStatus=0;
 
 function navigationDraw(){
     if (menuStatus==0){
-        document.getElementById("bar1").style.transform="rotate(45deg) translate(2px, 6.5px)";
+        document.getElementById("bar1").style.transform="rotate(45deg) translate(5.6px , 5.6px)";
         document.getElementById("bar2").style.opacity="0";
-        document.getElementById("bar3").style.transform="rotate(-45deg) translate(2px, -6.5px)";
+        document.getElementById("bar3").style.transform="rotate(-45deg) translate(5.6px , -5.6px)";
+        document.getElementById("categories").style.opacity="100";
         menuStatus = 1;
     }
     else {
         document.getElementById("bar1").style.transform="rotate(0) translate( 0, 0)";
         document.getElementById("bar2").style.opacity="100%";
         document.getElementById("bar3").style.transform="rotate(0) translate( 0, 0)";
+        document.getElementById("categories").style.opacity="0";
         menuStatus = 0;
     }
 }
