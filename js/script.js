66d71e0767c0a02efcf28299f8bff9905ca1836f
Anurag <anuraglama5@gmail.com>
2022-08-11 15:41:36 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-var drawerStatus = false;
+var bookId=0;
+var bookLog=["Book 1", "Author 1", 150] ["Book 2", "Author 2", 200];
+
 var loginStatus;
-if (loginStatus){
-    loginStatus=localStorage.getItem('_loginStatus');
+var level;
+
+if (localStorage.getItem("_loginStatus")){
+    loginStatus=localStorage.getItem("_loginStatus");
 }
 else{
-    localStorage.setItem('_loginStatus','false');
-    loginStatus=localStorage.getItem('_loginStatus');
+    localStorage.setItem("_loginStatus","false");
+    loginStatus=localStorage.getItem("_loginStatus");
 }
+
+var drawerStatus = false;
+
+
+var bookName, BookAuthor, bookCount, bookAmount;
+
 var user=["admin","admin"];
-console.log(loginStatus);
 
 function openDrawer(){
     if(drawerStatus==false){
@@ -29,16 +38,59 @@ function openDrawer(){
     }
 }
 
-function addToCart(){
+function accessCart(){
     console.log(loginStatus);
     if (loginStatus=="false"){
-        var col =document.querySelectorAll(".cardsAdd");
-        col.forEach((lacksLogin) => {
-            lacksLogin.style.animation="cardNudge 1s";
-            setTimeout(function(){lacksLogin.style.animation="none";},500);
-          });
+        document.getElementById("cartIconContainer").style.animation="cardNudge 1s";
+        setTimeout(function(){
+            document.getElementById("cartIconContainer").style.animation="none";
+        },1000)
     }
     else{
-
+        window.open("page/cart.html", "_parent");
     }
 }
+
+function AddToCart(){
+    console.log(loginStatus);
+    if (loginStatus=="false"){
+        accessCart();
+    }
+    else{
+        window.open("page/cart.html", "_parent");
+    }
+}
+
+function home(level){
+    if (level==0){
+        window.open("index.html","_parent");
+    }
+    else if (level==1){
+        window.open("../index.html", "_parent");
+    }
+
+}
+
+function accessAccount(){
+    var getUsername=document.getElementById("username").value;
+    var getPassword=document.getElementById("password").value;
+
+    if (getUsername=="admin"){
+        if(getPassword=="admin"){
+            localStorage.setItem("_loginStatus","true");
+            location.reload();
+        }
+        else{
+            document.getElementById("invalidPassword").style.display="flex";
+        }
+    }
+    else{
+        document.getElementById("invalidUsername").style.display="flex";
+    }
+}
+
+function logout(){
+    localStorage.setItem("_loginStatus","false");
+    location.reload();
+}
+
