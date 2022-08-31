1d9dee32ad1af332890adbb74c4564f210cf0587
Anurag <anuraglama5@gmail.com>
2022-08-31 08:23:35 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 var bookId=0;
 var bookLog=["Book 1", "Author 1", 150] ["Book 2", "Author 2", 200];
-var currenttime= new Date()/1000;
+var collapseCat;
+var bookColCheck=false;
+var aboutColCheck=false;
+var artColCheck=false;
 var loginStatus;
-var cartItemCount;
+var cartItemCount=0;
 var switchAccountId;
+localStorage.setItem("_cartItemCount", "0");
 
-var users=[["admin","admin","@admin","admin","admin@admin.localhost","red"],["bukant", "bukant","@BUKANT","Bukant", "www.bukant.com", "darkturquoise"]];
+var users=[["admin","admin","@admin","admin","admin@admin.localhost","red"],["bukant", "bukant","@BUKANT","Bukant", "www.bukant.com", "darkturquoise",]];
 localStorage.setItem("_users", JSON.stringify(users))
 localStorage.setItem("_usercount", "2");
 
 var level;
 
-if (localStorage.getItem("_loginStatus")){
+if (localStorage.getItem("_loginStatus")){ 
     loginStatus=localStorage.getItem("_loginStatus");
+    cartItemCount=parseInt(localStorage.getItem("_loginStatus"));
 }
 else{
     localStorage.setItem("_loginStatus","false");
@@ -22,32 +27,81 @@ else{
     localStorage.setItem("_email", "null");
     localStorage.setItem("_userimage", "NULL");
     loginStatus=localStorage.getItem("_loginStatus");
-    localStorage.setItem("_cartItemCount", "0");
 }
 
 var drawerStatus = false;
 
 
+function collapse(collapseCat){
+    if (collapseCat=="booksCategory"){
+        if (bookColCheck==false){
+            document.getElementById("booksCategory").style.display="flex";
+            document.getElementById("booksAdd").style.transform="rotate(45deg)";
+            document.getElementById("booksAdd").style.color="skyblue";
+            bookColCheck=true;
+        }
+        else{
+            document.getElementById("booksCategory").style.display="none";
+            document.getElementById("booksAdd").style.transform="rotate(0deg)";
+            document.getElementById("booksAdd").style.color="white";
+            bookColCheck=false;
+        }
+    }
+    else if (collapseCat=="aboutCategory"){
+        if (aboutColCheck==false){
+            document.getElementById("aboutCategory").style.display="flex";
+            document.getElementById("aboutAdd").style.transform="rotate(45deg)";
+            document.getElementById("aboutAdd").style.color="skyblue";
+            aboutColCheck=true;
+        }
+        else{
+            document.getElementById("aboutCategory").style.display="none";
+            document.getElementById("aboutAdd").style.transform="rotate(0deg)";
+            document.getElementById("aboutAdd").style.color="white";
+            aboutColCheck=false;
+        }
+    }
+    else if (collapseCat=="artCategory"){
+        if (artColCheck==false){
+            document.getElementById("artCategory").style.display="flex";
+            document.getElementById("artAdd").style.transform="rotate(45deg)";
+            document.getElementById("artAdd").style.color="skyblue";
+            artColCheck=true;
+        }
+        else{
+            document.getElementById("artCategory").style.display="none";
+            document.getElementById("artAdd").style.transform="rotate(0deg)";
+            document.getElementById("artAdd").style.color="white";
+            artColCheck=false;
+        }
+    }
+}
 
 function checkCart(){
     cartItemCount= parseInt(localStorage.getItem("_cartItemCount"));
     console.log(cartItemCount);
-    if (cartItemCount==0){
+    if(loginStatus=="false"){
         document.getElementById("cartCounter").style.display="none";
+        document.getElementById("cartCounterDisplay").textContent="";
     }
-    else if (cartItemCount>0&&cartItemCount<=9){
-        document.getElementById("cartCounter").style.display="flex";
-        document.getElementById("cartCounterDisplay").textContent=cartItemCount;
-    }
-    else if(cartItemCount>9){
-        localStorage.setItem("_cartItemCount", 9);
-        cartItemCount= parseInt(localStorage.getItem("_cartItemCount"));
-        document.getElementById("cartCounter").style.display="flex";
-        document.getElementById("cartCounterDisplay").textContent=cartItemCount;
-        document.getElementById("cartIconContainer").style.animation="cardNudge 1s";
-        setTimeout(function(){
-            document.getElementById("cartIconContainer").style.animation="none";
-        },1000)
+    else{
+        if (cartItemCount==0){
+            document.getElementById("cartCounter").style.display="none";
+        }
+        else if (cartItemCount>0&&cartItemCount<=9){
+            document.getElementById("cartCounter").style.display="flex";
+            document.getElementById("cartCounterDisplay").textContent=cartItemCount;
+        }
+        else if(cartItemCount>9){
+            localStorage.setItem("_cartItemCount", 9);
+            cartItemCount= parseInt(localStorage.getItem("_cartItemCount"));
+            document.getElementById("cartCounter").style.display="flex";
+            document.getElementById("cartCounterDisplay").textContent=cartItemCount;
+            document.getElementById("cartIconContainer").style.animation="cardNudge 1s";
+            setTimeout(function(){
+                document.getElementById("cartIconContainer").style.animation="none";
+            },1000)
+        }
     }
 }
 
@@ -57,6 +111,7 @@ function openDrawer(){
         document.getElementById("menubar2").style.transform="rotate(-45deg) translate(2px,-2px)";
         document.getElementById("categoriesMobile").style.top="0";
         document.getElementById("body").style.overflowY="hidden";
+        document.getElementById("categoriesMobile").style.overflowY="scroll";
 
         drawerStatus=true;
     }
@@ -65,6 +120,7 @@ function openDrawer(){
         document.getElementById("menubar2").style.transform="rotate(0)";
         document.getElementById("categoriesMobile").style.top="100%";
         document.getElementById("body").style.overflowY="scroll";
+        document.getElementById("categoriesMobile").style.overflowY="hidden";
 
         drawerStatus=false;
     }
@@ -132,6 +188,7 @@ function accessAccount(){
                 localStorage.setItem("_loginStatus","true");
                 localStorage.setItem("_loginAccess",codei);
                 location.reload();
+                document.title=getUsername+" | BUKANT";
                 break;
             }
             else{
@@ -176,6 +233,7 @@ function switchAccount(switchAccountId){
         setTimeout(() => {
             document.getElementById("loginForm").style.opacity="100%";
         }, 500);
+        document.title="Login | BUKANT";
     }
     else if(switchAccountId=="register"){
         document.getElementById("loginForm").style.display="none";
@@ -184,6 +242,7 @@ function switchAccount(switchAccountId){
         setTimeout(() => {
             document.getElementById("regForm").style.opacity="100%";
         }, 500);
+        document.title="Register | BUKANT";
     }
 }
 
